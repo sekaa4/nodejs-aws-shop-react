6f4118a1f356be65152ca9ee409f9f37b2de258c
Sergey P. <s.pansevi4@gmail.com>
2023-11-12 18:39:06 +0300

    feat: add prettier to the project

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  tabs: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
